fbdb50225ee52273f9743da7b08a7d4d9189bcea
David Strawn <davstrawn@jackhenry.com>
2025-12-17 15:28:44 -0600

    Make enableNotifications readOnly on darwin

--- modules/services/lorri.nix
+++ modules/services/lorri.nix
@@ -18,7 +18,13 @@ in
   options.services.lorri = {
     enable = lib.mkEnableOption "lorri build daemon";
 
-    enableNotifications = lib.mkEnableOption "lorri build notifications";
+    enableNotifications = lib.mkOption {
+      default = false;
+      example = true;
+      description = "Whether to enable lorri build notifications.";
+      type = lib.types.bool;
+      readOnly = pkgs.stdenv.isDarwin;
+    };
 
     package = lib.mkPackageOption pkgs "lorri" { };
 
